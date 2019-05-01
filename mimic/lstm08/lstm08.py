2d9d9a129fbb3197f43b9bcf53e4d3ef9ca6f94d
tacchang <jiro.kaihatsu@gmail.com>
2019-05-01 23:06:52 +0900

    多変量LSTMのサンプルを追加した。

---
+++ mimic/lstm08/lstm08.py
@@ -0,0 +1,120 @@
+# コード
+# https://qiita.com/tizuo/items/b9af70e8cdc7fb69397f
+# データ
+# https://oku.edu.mie-u.ac.jp/~okumura/stat/160118.html
+
+import numpy
+import matplotlib.pyplot as plt
+import pandas
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+import matplotlib
+
+matplotlib.use('TkAgg')
+
+# -------------------------------------------------------------
+
+# データ読み込み　Yは最初の列に配置する
+dataframe = pandas.read_csv('tokyo-weather-2003-2012.csv', usecols=[0, 3, 4, 5, 6], engine='python', skipfooter=1)
+plt.plot(dataframe)
+plt.show()
+print(dataframe.head())
+
+# -------------------------------------------------------------
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
+print(len(train), len(test))
+
+
+# -------------------------------------------------------------
+
+# convert an array of values into a dataset matrix
+# if you give look_back 3, a part of the array will be like this: Jan, Feb, Mar
+def create_dataset(dataset, look_back=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset) - look_back - 1):
+        xset = []
+        for j in range(dataset.shape[1]):
+            a = dataset[i:(i + look_back), j]
+            xset.append(a)
+        dataY.append(dataset[i + look_back, 0])
+        dataX.append(xset)
+    return numpy.array(dataX), numpy.array(dataY)
+
+
+# reshape into X=t and Y=t+1
+look_back = 12
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+print(testX.shape)
+print(testX[0])
+print(testY)
+
+# reshape input to be [samples, time steps(number of variables), features] *convert time series into column
+trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], trainX.shape[2]))
+testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], testX.shape[2]))
+
+# -------------------------------------------------------------
+
+# create and fit the LSTM network
+model = Sequential()
+model.add(LSTM(4, input_shape=(testX.shape[1], look_back)))  # shape：変数数、遡る時間数
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, epochs=200, batch_size=1, verbose=2)
+
+# -------------------------------------------------------------
+
+# make predictions
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+pad_col = numpy.zeros(dataset.shape[1] - 1)
+
+
+# invert predictions
+def pad_array(val):
+    return numpy.array([numpy.insert(pad_col, 0, x) for x in val])
+
+
+trainPredict = scaler.inverse_transform(pad_array(trainPredict))
+trainY = scaler.inverse_transform(pad_array(trainY))
+testPredict = scaler.inverse_transform(pad_array(testPredict))
+testY = scaler.inverse_transform(pad_array(testY))
+
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[:, 0], trainPredict[:, 0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[:, 0], testPredict[:, 0]))
+print('Test Score: %.2f RMSE' % (testScore))
+
+# -------------------------------------------------------------
+
+print(testY[:, 0])
+print(testPredict[:, 0])
+# shift train predictions for plotting
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict) + look_back, :] = trainPredict
+# shift test predictions for plotting
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict) + (look_back * 2) + 1:len(dataset) - 1, :] = testPredict
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(dataset))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
--- test_data.py
+++ test_data.py
@@ -1,89 +1,62 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
 
 import matplotlib
+
 matplotlib.use('TkAgg')
 
-_MAX_LEN = 50
-_CYCLE = 2  # 描画する周期の個数
-_PERIOD = 100  # 周期
+
+def get_begin():
+    return 0
 
 
-def noisy_cos(T=_PERIOD, ampl=0.05, cycle=2):
-    """
-    ノイズ入りcos波
-    :param T:
-    :param ampl: ノイズの程度
-    :param cycle: 描画するグラフの周期数
-    :return:
-    """
-    t = np.arange(0, cycle * 2 * T + 1)
-    return np.cos(2 * np.pi * t / T) + ampl * np.random.uniform(-1.0, 1.0, len(t))
+def get_end(cycle):
+    return 2 * np.pi * cycle
 
 
-def sin_cos(T=_PERIOD):
-    x = np.arange(0, 2 * T + 1)
-    # print(x)
-    # plt.plot(x, linestyle='dotted', color='#aaaaaa', label=u"正規のcos波")
-    # plt.show()
-    # print(x.shape)  # (201,)
+def synthetic_wave(x, T):
+    return np.sin(5 * np.pi * x / T) + np.sin(7 * np.pi * x / T) + np.sin(3 * np.pi * x / T) * 1.5
+    # return np.sin(5 * np.pi * x / T)
 
-    _data = np.concatenate(
-        [
-            np.sin(2.0 * np.pi * x / T).reshape(-1, 1),
-            np.cos(2.0 * np.pi * x / T).reshape(-1, 1)
-        ],
-        axis=1)
-    # print(_data)
-    # plt.plot(_data, linestyle='dotted', color='#aaaaaa', label=u"正規のcos波")
-    # plt.show()
-    # print(_data.shape)  # (201, 2)
+
+def synthetic_wave_data(cycle=1, n=100):
+    _x = np.linspace(get_begin(), get_end(cycle), n)
+    _y = pd.DataFrame({'wave': synthetic_wave(_x, n)})
+    return _y
+
+
+def sin_wave(begin=0, cycle=1, n=100):
+    _x = np.linspace(begin, 2 * np.pi * cycle, n)
+    _y = pd.Series(np.sin(_x), index=list(range(n)))
+    _data = pd.DataFrame({'wave': _y})
     return _data
 
 
-def get_data_dont_use():
-    # データセット作り
-    _data = sin_cos()
-
-    timesteps = 10
-    data_dim = _data.shape[1]
-
-    lstm_data = []
-    index_data = []
-
-    for i in range(timesteps):
-        length = _data[i:-1].shape[0] // timesteps
-        lstm_data.append(_data[i:i + length * timesteps].reshape(length, timesteps, data_dim))
-        index_data.append(np.arange(i, i + (length * timesteps), timesteps))
-
-    lstm_data = np.concatenate(lstm_data, axis=0)
-    print(type(lstm_data))
-    plt.plot(lstm_data[:, 0], linestyle='dotted', color='#aaaaaa', label=u"正規のcos波")  # 正規のcos波
-    plt.show()
-
-    index_data = np.concatenate(index_data, axis=0)
-    lstm_data = lstm_data[pd.Series(index_data).sort_values().index]
-    # plt.plot(lstm_data[0][0], linestyle='dotted', color='#aaaaaa', label=u"正規のcos波")  # 正規のcos波
-    # plt.show()
-
-    lstm_data_x = lstm_data[:, :-1, :]
-    lstm_data_y = lstm_data[:, -1, :]
-
-    return lstm_data
+def noisy_sin_wave(cycle=1, n=100):
+    # _s = sin_wave(begin, cycle, n)
+    # _noise = pd.Series(_s['wave'] + 0.05 * np.random.randn(n), index=list(range(n)))
+    _s = synthetic_wave_data(cycle, n)
+    _noise = pd.Series(_s['wave'] + 0.05 * np.random.randn(n), index=list(range(n)))
+    _n = pd.DataFrame({'noisy wave': _noise})
+    _data = pd.concat([_s, _n], axis=1)
+    return _data
 
 
 def get_data_for_lstm():
     # windowを設定
     _WINDOW_LEN = 10
 
-    df = noisy_sin_wave(begin=0, cycle=4, n=100)
-    df = (df - df.min()) / (df.max() - df.min())
+    # f = toy_problem(150)
+    # plt.plot(f)
+    # plt.show()
+
+    df = noisy_sin_wave(cycle=120, n=200)
+    df = (df - df.min()) / (df.max() - df.min())  # min-max normalization
     # print('----')
     # print(df)
-    # df.plot()
-    # plt.show()
+    df.plot()
+    plt.show()
 
     _lstm_in = []
     _data = pd.DataFrame({'noisy wave': df['noisy wave']})
@@ -98,21 +71,6 @@ def get_data_for_lstm():
     _lstm_out = np.array(_lstm_out)
 
     return _lstm_in, _lstm_out
-
-
-def sin_wave(begin=0, cycle=1, n=100):
-    _x = np.linspace(begin, 2 * np.pi * cycle, n)
-    _y = pd.Series(np.sin(_x), index=list(range(n)))
-    _data = pd.DataFrame({'wave': _y})
-    return _data
-
-
-def noisy_sin_wave(begin=0, cycle=1, n=100):
-    _s = sin_wave(begin, cycle, n)
-    _noise = pd.Series(np.sin(_s['wave']) + 0.1 * np.random.randn(n), index=list(range(n)))
-    _n = pd.DataFrame({'noisy wave': _noise})
-    _data = pd.concat([_s, _n], axis=1)
-    return _data
 
 
 ##############
